c5d3b8e409bd5770e94b2da82cadc57cfd3470ec
Suhail Abdi <suhailabdi857@gmail.com>
2025-06-27 10:53:34 +0300

    added the components of dashboard and the hero components

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+function Dashboard(){
+    return(
+        <div className="dashboard">
+            <h1 className="h1-t">travel</h1>
+            <div className="nav">
+                <p>Services</p>
+                <p>Destinations</p>
+                <p>Newsletter</p>
+                <p>Sitemap</p>
+            </div>
+            <button className="btn">Sign up</button>
+        </div>
+    )
+}
+export default Dashboard;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import traveller from "../assets/traveller.png"
+function Hero(){
+    return(
+        <div className="hero">
+            <div className="hero-content">
+                <h3 className="hero-tagline">Best Destinations around the world</h3>
+                <h1 className="hero-title">Travel, enjoy and live a new and full life</h1>
+                <p className="hero-desc">Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
+                <div className="hero-btns">
+                    <button className="find-more">Find Out More</button>
+                    <button className="play"> Play Demo</button>
+                </div>
+            </div>
+            <div className="hero-img-container">
+                <img className="hero-img" src={traveller} alt="image of a female traveller"  />
+            </div>
+    </div>
+    )
+    
+}
+export default Hero;
